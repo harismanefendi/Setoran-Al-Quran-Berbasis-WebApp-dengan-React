30828c50dad12a3aab62bfb96d0f746dbf74d00c
Harisman Efendi <harismanef@gmail.com>
2024-01-11 05:49:14 +0700

    perubahan

--- src/pages/HomeSiswa/Home.jsx
+++ src/pages/HomeSiswa/Home.jsx
@@ -10,19 +10,15 @@ function Home() {
     const timer = setTimeout(() => {
       setShowAlert(false);
     }, 5000);
-
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-purple-100">
       <div className="mt-4 text-center">
         <h1 className="text-blue-800 font-bold text-4xl md:text-6xl relative">
-          <span className="absolute top-0 left-0 -z-1 text-red-800">
+          <span className="text-blue-600">
             HSQOnline <br /> Hayyuk Setor Quran Online
-          </span>
-          <span className="text-blue-400">
-            <span className="arabic-text">HSQOnline</span> <br /> <span className="arabic-text">Hayyuk Setor Quran Online</span>
           </span>
         </h1>
       </div>
@@ -30,15 +26,15 @@ function Home() {
       <p className="mt-4 text-base md:text-2xl lg:text-3xl text-center">Assalamu'alaikum Warahmatullahi Wabarakatuh,</p>
       <p className="text-lg font-semibold text-center">Selamat datang di Halaman Utama Setoran Hafalan.</p>
       <p className="text-lg font-semibold text-center">Kami mengundang Anda untuk menjelajahi beragam fitur yang kami tawarkan, sesuaikan dengan kebutuhan Anda.</p>
-      <div className="mt-4 flex flex-col space-y-2">
-        <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:shadow-lg transform hover:-translate-y-1 transition duration-300" onClick={() => navigate("/setoran")}>
+      <div className="mt-4 flex flex-col space-y-4">
+        <button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:shadow-xl transform hover:scale-105 transition duration-500 ease-in-out" onClick={() => navigate("/setoran")}>
           Setoran Rekaman Hafalan
         </button>
-        <button className="px-4 py-2 bg-green-500 text-white rounded-lg hover:shadow-lg transform hover:-translate-y-1 transition duration-300">Tips Mudah Menghafal</button>
-        <button className="px-4 py-2 bg-red-500 text-white rounded-lg hover:shadow-lg transform hover:-translate-y-1 transition duration-300">Ajukan Setoran Via Online</button>
-        <button className="px-4 py-2 bg-purple-500 text-white rounded-lg hover:shadow-lg transform hover:-translate-y-1 transition duration-300">Tombol 4</button>
+        <button className="px-6 py-3 bg-green-500 text-white rounded-lg shadow-md hover:shadow-xl transform hover:scale-105 transition duration-500 ease-in-out">Tips Mudah Menghafal</button>
+        <button className="px-6 py-3 bg-red-500 text-white rounded-lg shadow-md hover:shadow-xl transform hover:scale-105 transition duration-500 ease-in-out">Ajukan Setoran Via Online</button>
+        <button className="px-6 py-3 bg-purple-500 text-white rounded-lg shadow-md hover:shadow-xl transform hover:scale-105 transition duration-500 ease-in-out">Tombol 4</button>
       </div>
-      <div className="fixed bottom-0 left-0 w-full bg-gray-200 py-4">
+      <div className="fixed bottom-0 left-0 w-full bg-blue-200 py-4">
         <div className="container mx-auto flex justify-center space-x-4">
           <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:shadow-lg transform hover:-translate-y-1 transition duration-300" onClick={() => navigate("/feedback-siswa", { state: { userEmail: "email_siswa_di_sini" } })}>
             Feedback
